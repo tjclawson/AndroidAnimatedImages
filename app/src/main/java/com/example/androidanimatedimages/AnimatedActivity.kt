d439008c85f0a4b14c3a883fbbcc92af18ebc1a9
tjclawson <tjamesclawson@gmail.com>
2019-08-21 16:20:23 -0400

    added gif and animated drawable to activities

--- app/src/main/java/com/example/androidanimatedimages/AnimatedActivity.kt
+++ app/src/main/java/com/example/androidanimatedimages/AnimatedActivity.kt
@@ -1,12 +1,19 @@
 package com.example.androidanimatedimages
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_animated.*
 
 class AnimatedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animated)
+
+        val animatedDrawable = ContextCompat.getDrawable(this, R.drawable.elmo_animated)
+        image_animated.setImageDrawable(animatedDrawable)
+        (animatedDrawable as AnimationDrawable).start()
     }
 }
